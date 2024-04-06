321f32c584501a7d6ae5131f8e333b43a2304ce3
ranggaggngntt <ranggaakb48@yahoo.com>
2024-04-06 09:15:26 -0700

    first commit

---
+++ src/main/java/com/mycompany/datadiri/DataDiri.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.datadiri;
+/**
+ *
+ * @author USER
+ */
+public class DataDiri {
+
+    public static void main(String[] args) {
+        Form form = new Form();
+        
+        form.show();
+    }
+}
